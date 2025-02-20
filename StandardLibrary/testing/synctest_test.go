6c66c673295317eb1edbd3c2e798cabb336d37c2
Andrew Phillips <andrewp@dataprocessors.com.au>
2025-02-21 09:38:18 +1100

    simple syntest.Run() test

--- StandardLibrary/testing/synctest_test.go
+++ StandardLibrary/testing/synctest_test.go
@@ -1,1 +1,16 @@
-package testing
+//go:build go1.4
+
+package __
+
+import (
+	"fmt"
+	"testing"
+	"testing/synctest" // requires GOEXPERIMENT=synctest
+	"time"
+)
+
+func TestSyncTest(t *testing.T) {
+	synctest.Run(func() {
+		fmt.Println(time.Now())
+	})
+}
