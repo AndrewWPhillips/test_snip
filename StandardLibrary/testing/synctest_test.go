3765919e9c1dfaa5a4149bc4bb89377d1129f3d4
Andrew Phillips <andrewp@dataprocessors.com.au>
2025-02-24 13:44:14 +1100

    Test of Go 1.24 syncTest

--- StandardLibrary/testing/synctest_test.go
+++ StandardLibrary/testing/synctest_test.go
@@ -1,4 +1,4 @@
-//go:build go1.4
+//go:build go1.24
 
 package __
 
@@ -10,7 +10,42 @@ import (
 )
 
 func TestSyncTest(t *testing.T) {
+	start := time.Now()
 	synctest.Run(func() {
-		fmt.Println(time.Now())
+		go func() {
+			fmt.Println("A:", time.Now())
+			time.Sleep(10 * time.Second)
+			fmt.Println("B:", time.Now())
+		}()
+		time.Sleep(time.Second)
+		fmt.Println("C:", time.Now())
 	})
+	fmt.Println("took:", time.Now().Sub(start))
+}
+
+func TestSyncExternal(t *testing.T) {
+	start := time.Now()
+
+	// Create a channel and start a go-routine to write to it
+	ch := make(chan struct{})
+	go func() {
+		for range 5 {
+			time.Sleep(time.Second)
+			ch <- struct{}{}
+		}
+		close(ch)
+	}()
+
+	synctest.Run(func() {
+		open := true
+		// loop till the channel is closed
+		for open {
+			// get the next one from the channel
+			select {
+			case _, open = <-ch:
+			}
+			fmt.Println(time.Now())
+		}
+	})
+	fmt.Println("took:", time.Now().Sub(start))
 }
